d1b2355d8ba7649068a8946e1e808653a498033e
nice <zhz941009@163.com>
2022-11-27 13:22:37 +0800

    init project

---
+++ src/main/java/com/jad/jad/JadApplication.java
@@ -0,0 +1,13 @@
+package com.jad.jad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jad/jad/JadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jad.jad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
